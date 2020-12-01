1d805cf83096112a552a50f5e610f3574e40b62b
Godfrey Best <godfrey.c.best@gmail.com>
2020-12-01 15:43:59 -0500

    reactive connection to db

---
+++ imports/ui/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { useTracker } from 'meteor/rdb:svelte-meteor-data';
+  import { Order, Orders } from '../api/orders';
+
+  // when order added to database this will fetch all of them from DB
+  $: orders = useTracker(() => Orders.find().fetch());
+</script>
+
+<header>
+  <h1>Yerrr</h1>
+  {#each $orders as order}
+    <h2>{order.username}</h2>
+    <p>${order.amount}</p>
+  {/each}
+</header>
