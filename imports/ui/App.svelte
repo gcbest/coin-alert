673e08887893f5bb220fc023e683b7f0370e46ed
Godfrey Best <godfrey.c.best@gmail.com>
2020-12-01 21:33:27 -0500

    add user auth

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -1,15 +1,42 @@
 <script>
   import { useTracker } from 'meteor/rdb:svelte-meteor-data';
-  import { Order, Orders } from '../api/orders';
+  import { Orders } from '../api/orders';
+  import { LoginWindow, Logout } from 'meteor/levelup:svelte-accounts-ui';
+  import Order from '../ui/orders/Order.svelte';
+
+  let username = '';
+  let amount = 0;
 
   // when order added to database this will fetch all of them from DB
   $: orders = useTracker(() => Orders.find().fetch());
+  $: user = useTracker(() => Meteor.user());
+  $: userId = useTracker(() => Meteor.userId());
+
+  function handleSubmit(event) {
+    console.log($user);
+    Orders.insert({
+      email: $user.emails[0].address,
+      amount,
+    });
+
+    username = '';
+  }
 </script>
 
 <header>
+  {#if $userId}
+    <Logout />
+  {:else}
+    <LoginWindow />
+  {/if}
   <h1>Yerrr</h1>
   {#each $orders as order}
-    <h2>{order.username}</h2>
-    <p>${order.amount}</p>
+    <Order {order} />
   {/each}
+
+  <form on:submit|preventDefault={handleSubmit}>
+    <input type="text" name="username" bind:value={username} />
+    <input type="number" name="amount" bind:value={amount} />
+    <button type="submit">Order</button>
+  </form>
 </header>
---
+++ imports/ui/orders/Order.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let order;
+  import { Orders } from '../../api/orders';
+  function deleteOrder() {
+    Orders.remove(order._id);
+  }
+</script>
+
+<li>
+  {order.email}
+  | ${order.amount}
+  <button on:click={deleteOrder}>Delete</button>
+</li>
